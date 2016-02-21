958fcf7b177a6c24cc0a7228f6ec16662f7c3514
Giedrius Kartanovic <giedrius@websitevision.co.uk>
2016-02-21 21:13:09 +0000

    Loop Speed Change mode saves new bpm

--- src/simori/LoopSpeedChangeMode.java
+++ src/simori/LoopSpeedChangeMode.java
@@ -3,14 +3,45 @@ package simori;
 public class LoopSpeedChangeMode implements Mode
 {
 
+	private int speed;
+
+	public LoopSpeedChangeMode()
+	{
+		if (SimoriOn.getClockHand() != null) {
+			SimoriOn.getClockHand().running.set(false);
+		}
+	}
+
 	@Override
 	public void processMatrixButton(GridButton button) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Matrix button processed in Loop Speed Change Mode");
+
+		// Highlight row and column of the button
+		button.getGUI().highlightColumnAndRow(button.getCoordsX(), button.getCoordsY());
+
+		// Set speed variable
+		if (button.getCoordsX() * button.getCoordsY() <= 160) {
+			this.speed = button.getCoordsX() * button.getCoordsY();
+		} else {
+			this.speed = 160;
+		}
+
+		// Write out speed onto the LCD
+		SimoriOn.getInstance().getGui().writeToLCD(Integer.toString(this.speed));
+
+		System.out.println("BPM Selected: " + this.speed);
 		
 	}
 
 	public void processOKButton(){
-		System.out.println("OK Button pressed in Voice Change Mode");
+		System.out.println("SET BPM To " + this.speed);
+
+		SimoriOn.getInstance().getGui().LCD.setText(null);
+
+		SimoriOn.getInstance().setBPM(this.speed);
+		SimoriOn.getInstance().setMode(new PerformanceMode());
+		SimoriOn.getInstance().getGui().turnOffFunctionButtons();
 	}
 
 }
