bd9cb45ebb51d769df633b66fce7f04ea31077a1
Carl Saptarshi <carlsaptarshi@Carls-MacBook-Pro.local>
2016-02-25 01:39:26 +0000

    GB Junit testing

--- src/JUnit/GridButtonTest.java
+++ src/JUnit/GridButtonTest.java
@@ -3,15 +3,34 @@ package JUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class GridButtonTest {
 
+public SimoriOn test;
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception
+	{
+		test = SimoriOn.getInstance();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	
+	@Test
+	public void testGdridButtonConstructor(int x, int y){
+		GridButton gb = new GridButton(x,y);
+		GridButton egb = new GridButton(x,y);
+		
+		assertEquals(gb,egb);
+	}
+	
+	@Test
+	public void testValidButton(){
+		assertEquals((3,4),(3,4));
+	}
+	
 }
