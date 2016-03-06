037bd2ebb158b55721f78b4970e2c601b36a2670
alxsah <alex455@hotmail.co.uk>
2016-03-06 02:52:01 +0000

    R2 3x3 square letter selection

--- src/simori/SaveConfigurationMode.java
+++ src/simori/SaveConfigurationMode.java
@@ -3,10 +3,39 @@ package simori;
 public class SaveConfigurationMode implements Mode
 {
 
+	public SaveConfigurationMode(){
+		// Turn Off Clockhand
+		{
+			if (SimoriOn.getClockHand() != null) {
+				SimoriOn.getClockHand().running.set(false);
+			}
+		}
+	}
 	@Override
 	public void processMatrixButton(GridButton button) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Matrix button processed in Save Configuration Mode");
+
+		// Highlight row and column of the button
+		button.getGUI().highlightColumnAndRow(button.getCoordsX(),
+				button.getCoordsY());
+
+		char[][] alph = {{'a', 'b', 'c', 'd', 'e'}, {'f', 'g', 'h', 'i', 'j'},{ 'k', 'l', 'm', 'n', 'o'}, {'p'
+			,'q', 'r', 's', 't'},{ 'u', 'v', 'w', 'x', 'y'}, {'z'}};
+
+		int x = button.getCoordsX();
+		int y = button.getCoordsY();
+
+		if (x == 15 && y != 0){
+			SimoriOn.getInstance().getGui().writeToLCD(alph[(15 - y) / 3][4] + "");
+		}
+		else if (y == 0 && x > 2){
+			SimoriOn.getInstance().getGui().writeToLCD(alph[5][0] + "");
+		}
+		else {
+			SimoriOn.getInstance().getGui().writeToLCD(alph[(15 - y) / 3][x / 3] + "");
+		}
+
 	}
 
 	public void processOKButton(){
